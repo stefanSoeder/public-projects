6f925203029aef280d2e376ba8d05e9bcad8504f
stefanSoeder <stefanms3@gmail.com>
2024-06-18 17:28:23 +0400

    architectureInitTemplate

---
+++ gameshub/src/utils/initTemplate.js
@@ -0,0 +1,7 @@
+export const initTemplate = () => {
+  const app = document.getElementById("app");
+  const header = document.createElement("header");
+  const main = document.createElement("main");
+  const footer = document.createElement("footer");
+  app.append(header, main, footer);
+};
