798a8a86debd83d75d7449f14e4db0716a064abd
stefanSoeder <stefanms3@gmail.com>
2024-06-20 11:45:15 +0400

    axiosPokemonService

---
+++ gameshub/src/utils/axios.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+export const axiosUtil = async (options) => {
+  return await axios.request(options).then((res) => res.data);
+};
--- gameshub/src/utils/index.js
+++ gameshub/src/utils/index.js
@@ -1,3 +1,4 @@
 export * from "./initTemplate";
 export * from "./route";
 export * from "./changeColor";
+export * from "./axios";
