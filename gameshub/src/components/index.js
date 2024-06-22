b77419f17925e50e4c57eaee601ac6b3cd6bac18
stefanSoeder <stefanms3@gmail.com>
2024-06-22 12:11:29 +0400

    componentButtonFilter

--- gameshub/src/components/index.js
+++ gameshub/src/components/index.js
@@ -3,3 +3,4 @@ export * from "./Header/Header";
 export * from "./CardPokemons/CardPokemons";
 export * from "./Spinner/Spinner";
 export * from "./Spinner/SpinnerButton";
+export * from "./ButtonFilter/ButtonFilter";
